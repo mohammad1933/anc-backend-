3f203d11e212d290385d6d6963aa62ccc2ce2433
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-07-24 00:44:26 +0400

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/project-detail.js', 'resources/js/favorites.js', 'resources/js/recently-viewed.js', 'resources/js/rfq.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
